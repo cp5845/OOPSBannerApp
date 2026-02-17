d6833fc0bd8207ad78d66eb274690b0e0bf7b22e
cp5845 <cp5845@srmist.edu.in>
2026-02-17 10:14:31 +0530

    Add files via upload

---
+++ OOPS Banner App.java
@@ -0,0 +1,17 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "********************************",
+                "*                              *",
+                "*        WELCOME TO OOPS       *",
+                "*                              *",
+                "********************************"
+        };
+
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+    }
+}
